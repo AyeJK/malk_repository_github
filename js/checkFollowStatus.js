2eaf284a5f247ba739e1d8b0c9c033051bcbbaf3
Jeremy Kaye <j@jtkaye.com>
2024-03-14 14:25:39 -0700

    test

---
+++ js/checkFollowStatus.js
@@ -0,0 +1,28 @@
+//Serverless Function//
+const Airtable = require('airtable');
+
+exports.handler = async (event) => {
+    // Initialize Airtable
+    Airtable.configure({
+        endpointUrl: 'https://api.airtable.com',
+        apiKey: process.env.AIRTABLE_PAT // Use PAT stored in environment variable
+    });
+    const base = Airtable.base('appaFBiB1nQcgm9Oz'); // Replace with your Airtable Base ID
+
+    const { userId, fieldName } = event.queryStringParameters;
+
+    try {
+        const records = await base('Posts').select({
+            filterByFormula: `SEARCH("${userId}", {${fieldName}})`
+        }).firstPage();
+
+        const slugs = records.map(record => record.fields.Slug);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(slugs)
+        };
+    } catch (error) {
+        return { statusCode: 500, body: JSON.stringify({ error: error.toString() }) };
+    }
+};
+//test//
