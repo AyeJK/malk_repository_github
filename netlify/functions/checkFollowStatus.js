8720c6ab73b7817b979580c21c8196de2359c924
Jeremy Kaye <j@jtkaye.com>
2024-03-19 19:39:05 -0700

    returning to previous method

--- netlify/functions/checkFollowStatus.js
+++ netlify/functions/checkFollowStatus.js
@@ -2,37 +2,26 @@
 const Airtable = require('airtable');
 
 exports.handler = async (event) => {
+    // Initialize Airtable
     Airtable.configure({
         endpointUrl: 'https://api.airtable.com',
-        apiKey: process.env.Airtable_PAT
+        apiKey: process.env.Airtable_PAT 
     });
     const base = Airtable.base('appaFBiB1nQcgm9Oz');
 
-    const { userId } = event.queryStringParameters;
+    const { userId, fieldName } = event.queryStringParameters;
 
     try {
-        // Fetch posts where the user ID is present in either 'Followers' or 'User Likes'
         const records = await base('Posts').select({
-            filterByFormula: `OR(SEARCH("${userId}", {Followers}), SEARCH("${userId}", {User Likes}))`
+            filterByFormula: `SEARCH("${userId}", {${fieldName}})`
         }).firstPage();
 
-        const postsData = records.map(record => {
-            return {
-                postId: record.id, // Assuming you use the record ID as the post ID
-                likes: record.fields['User Likes'] && record.fields['User Likes'].includes(userId),
-                follows: record.fields['Followers'] && record.fields['Followers'].includes(userId)
-            };
-        });
-
+        const slugs = records.map(record => record.fields.Slug);
         return {
             statusCode: 200,
-            body: JSON.stringify(postsData)
+            body: JSON.stringify(slugs)
         };
     } catch (error) {
-        console.error('Error fetching data from Airtable:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: error.toString() })
-        };
+        return { statusCode: 500, body: JSON.stringify({ error: error.toString() }) };
     }
 };
