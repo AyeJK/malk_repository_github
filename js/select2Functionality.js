0b89ea5c7ad68792ef5036c5bfc9c0926f89aa5e
Jeremy Kaye <j@jtkaye.com>
2024-03-27 12:47:48 -0700

    select2Functionality

---
+++ js/select2Functionality.js
@@ -0,0 +1,21 @@
+  $('#video-tags').select2({
+      placeholder: 'Select tags',
+      tags: true,
+      maximumSelectionLength: 5,
+      tokenSeparators: [','],
+      createTag: function(params) {
+          // If there are no search terms, don't create a tag
+          if ($.trim(params.term) === '') {
+              return null;
+          }
+          // Prepend '#' to the tag
+          return {
+              id: '#' + $.trim(params.term),
+              text: '#' + $.trim(params.term)
+          };
+      }
+  });
+  $('#category').select2({
+      placeholder: 'Select a category',
+      minimumResultsForSearch: Infinity,
+  });
