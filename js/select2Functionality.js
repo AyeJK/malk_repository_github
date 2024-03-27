f688c9d59e96e23c946e0b1efb5ed0914220499e
Jeremy Kaye <j@jtkaye.com>
2024-03-27 16:51:29 -0700

    lowercase tags

--- js/select2Functionality.js
+++ js/select2Functionality.js
@@ -1,21 +1,24 @@
-  $('#video-tags').select2({
-      placeholder: 'Select tags',
-      tags: true,
-      maximumSelectionLength: 5,
-      tokenSeparators: [','],
-      createTag: function(params) {
-          // If there are no search terms, don't create a tag
-          if ($.trim(params.term) === '') {
-              return null;
-          }
-          // Prepend '#' to the tag
-          return {
-              id: '#' + $.trim(params.term),
-              text: '#' + $.trim(params.term)
-          };
-      }
-  });
-  $('#category').select2({
-      placeholder: 'Select a category',
-      minimumResultsForSearch: Infinity,
-  });
+$('#video-tags').select2({
+    placeholder: 'Select tags',
+    tags: true,
+    maximumSelectionLength: 5,
+    tokenSeparators: [','],
+    createTag: function(params) {
+        var term = $.trim(params.term).toLowerCase(); 
+
+        // If there are no search terms, don't create a tag
+        if (term === '') {
+            return null;
+        }
+
+        // Prepend '#'
+        return {
+            id: '#' + term, 
+            text: '#' + term 
+        };
+    }
+});
+$('#category').select2({
+    placeholder: 'Select a category',
+    minimumResultsForSearch: Infinity,
+});
