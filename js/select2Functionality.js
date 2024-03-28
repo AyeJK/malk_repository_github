f79112fbd67e048c6e22407fbdbbac0f358f8bad
Jeremy Kaye <j@jtkaye.com>
2024-03-27 17:05:50 -0700

    tag case change

--- js/select2Functionality.js
+++ js/select2Functionality.js
@@ -4,17 +4,25 @@ $('#video-tags').select2({
     maximumSelectionLength: 5,
     tokenSeparators: [','],
     createTag: function(params) {
-        var term = $.trim(params.term).toLowerCase(); 
+        var term = $.trim(params.term).toLowerCase(); // Ensure the tag is in lowercase
 
         // If there are no search terms, don't create a tag
         if (term === '') {
             return null;
         }
 
-        // Prepend '#'
+        // Prepend '#' to the lowercase tag and ensure it's unique in case-insensitive manner
+        var isDuplicate = $(this.$element[0]).find("option").filter(function() {
+            return this.text.toUpperCase() === ('#' + term).toUpperCase();
+        }).length;
+
+        if (isDuplicate) {
+            return null;
+        }
+
         return {
-            id: '#' + term, 
-            text: '#' + term 
+            id: '#' + term, // The ID is now based on the lowercase term
+            text: '#' + term // The displayed text is also based on the lowercase term
         };
     }
 });
