c12d6c59f17298b24f366def99040241a6a95e9c
Jeremy Kaye <j@jtkaye.com>
2024-03-27 18:22:12 -0700

    works

--- js/select2Functionality.js
+++ js/select2Functionality.js
@@ -4,18 +4,29 @@ $('#video-tags').select2({
     maximumSelectionLength: 5,
     tokenSeparators: [','],
     createTag: function(params) {
+        var term = $.trim(params.term).toLowerCase(); // Ensure the tag is in lowercase
+
         // If there are no search terms, don't create a tag
-        if ($.trim(params.term) === '') {
+        if (term === '') {
             return null;
         }
-        // Prepend '#' to the tag
+
+        // Prepend '#' to the lowercase tag and ensure it's unique in case-insensitive manner
+        var isDuplicate = $(this.$element[0]).find("option").filter(function() {
+            return this.text.toUpperCase() === ('#' + term).toUpperCase();
+        }).length;
+
+        if (isDuplicate) {
+            return null;
+        }
+
         return {
-            id: '#1' + $.trim(params.term),
-            text: '#1' + $.trim(params.term)
+            id: '#' + term, // The ID is now based on the lowercase term
+            text: '#' + term // The displayed text is also based on the lowercase term
         };
     }
 });
 $('#category').select2({
-    placeholder: 'Select a categoryz',
+    placeholder: 'Select a category',
     minimumResultsForSearch: Infinity,
 });
