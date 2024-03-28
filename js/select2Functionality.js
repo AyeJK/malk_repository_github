bd5be7c41fb358ce94036af07cd18002a37b5da5
Jeremy Kaye <j@jtkaye.com>
2024-03-27 17:23:16 -0700

    revert

--- js/select2Functionality.js
+++ js/select2Functionality.js
@@ -4,30 +4,15 @@ $('#video-tags').select2({
     maximumSelectionLength: 5,
     tokenSeparators: [','],
     createTag: function(params) {
-        // Debugging: Log the original term
-        console.log("Original term:", params.term);
-
-        // Trim the input term and convert it to lowercase
-        var term = $.trim(params.term).toLowerCase();
-
-        // Debugging: Log the processed term
-        console.log("Processed term:", term);
-
-        // If there are no search terms (after trimming and converting to lowercase), don't create a tag
-        if (term === '') {
+        // If there are no search terms, don't create a tag
+        if ($.trim(params.term) === '') {
             return null;
         }
-
-        // Prepend '#' to the lowercase tag
-        var tag = {
-            id: '#' + term, // The ID is now based on the lowercase term
-            text: '#' + term // The displayed text is also based on the lowercase term
+        // Prepend '#' to the tag
+        return {
+            id: '#' + $.trim(params.term),
+            text: '#' + $.trim(params.term)
         };
-
-        // Debugging: Log the created tag
-        console.log("Created tag:", tag);
-
-        return tag;
     }
 });
 $('#category').select2({
