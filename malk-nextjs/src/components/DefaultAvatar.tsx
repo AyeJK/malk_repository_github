fed0f75f1865a0825c60a7424c35839e9357adf7
Jeremy Kaye <j@jtkaye.com>
2025-05-01 19:15:23 -0700

    Update DefaultAvatar component to use consistent UserIcon instead of initials

--- malk-nextjs/src/components/DefaultAvatar.tsx
+++ malk-nextjs/src/components/DefaultAvatar.tsx
@@ -9,28 +9,9 @@ interface DefaultAvatarProps {
 }
 
 export default function DefaultAvatar({ userId, userName }: DefaultAvatarProps) {
-  const getInitials = (name: string) => {
-    return name
-      .split(' ')
-      .map(word => word[0])
-      .join('')
-      .toUpperCase()
-      .slice(0, 2);
-  };
-
-  if (!userName) {
-    return (
-      <div className="w-full h-full bg-gray-700 flex items-center justify-center">
-        <UserIcon className="w-4 h-4 text-gray-400" />
-      </div>
-    );
-  }
-
   return (
     <div className="w-full h-full bg-gray-700 flex items-center justify-center">
-      <span className="text-xs font-medium text-gray-300">
-        {getInitials(userName)}
-      </span>
+      <UserIcon className="w-2/3 h-2/3 text-gray-400" />
     </div>
   );
 } 
